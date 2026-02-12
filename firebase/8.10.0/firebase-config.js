edec78e174ecaaafbd587a40b160069e565dc323
dimwap <dimwap@mail.ru>
2026-02-12 16:12:51 +0300

    Create firebase-config.js

---
+++ firebase/8.10.0/firebase-config.js
@@ -0,0 +1,7 @@
+// firebase-config.js
+const firebaseConfig = {
+    projectId: "gitchat-2b675",
+    appId: "1:1021059658961:web:e455940894ca49673481f0",
+    apiKey: "AIzaSyBQ8AVQ1D8c0IpDgoRXL0QayaaVktd3_Xg",
+    authDomain: "gitchat-2b675.firebaseapp.com",
+    databaseURL: "https://gitchat-2b675-default-rtdb.firebaseio.com"};
