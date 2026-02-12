5274b11e504105aad96176b48fa11f369caf5d3f
dimwap <dimwap@mail.ru>
2026-02-12 19:36:07 +0300

    Update firebase-config.js

--- firebase/8.10.0/firebase-config.js
+++ firebase/8.10.0/firebase-config.js
@@ -2,6 +2,6 @@
 const firebaseConfig = {
     projectId: "gitchat-2b675",
     appId: "1:1021059658961:web:e455940894ca49673481f0",
-    apiKey: "AIzaSyBQ8AVQ1D8c0IpDgoRXL0QayaaVktd3_Xg",
+    apiKey: "AIzaSyAi1dNBPgW9Dlgrc_gxjG40JTucwJU1O00",
     authDomain: "gitchat-2b675.firebaseapp.com",
     databaseURL: "https://gitchat-2b675-default-rtdb.firebaseio.com"};
